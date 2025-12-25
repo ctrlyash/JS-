920c32543ff5e24971b7425f2806975a2922701a
ctrlyash <yashdadhich9934@gmail.com>
2025-12-25 12:49:12 +0000

    add dataTypes summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,73 @@
+// Two types of Data Types in JavaScript
+// 1. Primitive Data Types
+// 2. Reference Data Types (Non-primitive)
+
+
+// Primitive Data Types
+
+// 7 primitive data types in JavaScript
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. BigInt
+// 7. Symbol
+
+// Example of Primitive Data Types
+
+const age = 30; // Number
+const pi = 3.14; // Number 
+const name = "John"; // String
+const isStudent = false; // Boolean
+const address = null; // Null
+let phoneNumber; // Undefined
+const bigIntNumber = 9007199254741991n; // BigInt
+const uniqueId = Symbol("123"); // Symbol
+const anotherUniqueId = Symbol("123"); // Symbol
+
+console.log(uniqueId === anotherUniqueId); // false
+
+//JS is dynamically typed language as we do not need to specify data types explicitly
+
+
+
+// Reference Data Types (Non-primitive)
+
+// 1. Object
+// 2. Array
+// 3. Function
+
+// Example of Reference Data Types
+
+let myObject = { 
+    name: "Yash", 
+    age: 19 
+}; // Object
+
+const heros = ["Superman", "Batman", "Ironman"]; // Array
+
+const myFunction = function() { 
+    console.log("Hello World"); 
+}; // Function
+
+
+
+// Checking Data Types using typeof operator
+
+console.log(typeof bigIntNumber); // "bigint"
+
+console.log(typeof anotherUniqueId); // "symbol"
+
+console.log(typeof undefined); // "undefined"
+
+console.log(typeof null); // "object" (It's a known quirk in JavaScript)
+
+console.log(typeof myObject); // "object"
+
+console.log(typeof heros); // "object"
+
+console.log(typeof myFunction); // "function" (functions in JavaScript are a special type of object)
+
+//study more on data types here:
+// https://262.ecma-international.org/5.1/#sec-11.4.3
